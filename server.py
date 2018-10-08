35ac2f30de9a8cd86102d934cef5e5cce465b441
Selim Enes Kılıçaslan <33657157+SelimEnes@users.noreply.github.com>
2018-10-08 15:31:46 +0300

    Test

--- server.py
+++ server.py
@@ -1,16 +1,40 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home_page():
-    return "Ana Sayfa"
+def index():
+    return render_template('index.html')
 
-@app.route("/secondpage")
-def second_page():
-    return "İkinci Sayfa"
+@app.route("/main", methods = ['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        userName = request.form['nm']
+        return redirect(url_for('redirectUser', name = userName))
+    else:
+        userName = request.args.get('nm')
+        return redirect(url_for('redirectUser', name = userName))
+
+@app.route("/secondpage/<int:ID>/")
+def second_page(ID):
+    return "Ikinci Sayfa %d" % ID
+
+@app.route("/user/<name>/")
+def redirectUser(name):
+    if name == 'admin':
+        return redirect(url_for('adminPage'))
+    else:
+        return redirect(url_for('userPage', userName = name))
+
+@app.route("/admin_page/")
+def adminPage():
+    return "Welcome Admin!"
+
+@app.route("/user_page/<userName>/")
+def userPage(userName):
+    return "Welcome %s" % userName
 
 
 if __name__ == "__main__":
