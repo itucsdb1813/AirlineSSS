4d16df398fdf2f5b2535048b5241329037beb59f
Selim Enes Kılıçaslan <33657157+SelimEnes@users.noreply.github.com>
2018-10-10 16:33:38 +0300

    deneme

--- server.py
+++ server.py
@@ -1,14 +1,28 @@
 from flask import Flask, redirect, url_for, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:itucspw@localhost/itucsdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ddzwibxvysqwgx:9e0edae8756536ffdba78314ebde69e2d019e58a2c05dfbad508b5eb657ac9e7@ec2-54-247-101-205.eu-west-1.compute.amazonaws.com:5432/d8o6dthnk5anke'
+##app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:itucspw@localhost:65530/itucsdb'
+app.debug = True
 db = SQLAlchemy(app)
 
 _Dictionary = {
     "SelimK" : "aslan123",
     "admin"  : "admin"
 }
+
+class Users(db.Model):
+    Username = db.Column(db.String(20), primary_key = True)
+    Password = db.Column(db.String(20))
+
+    def __init__(self, _Username, _Password):
+        self.Username = _Username
+        self.Password = _Password
+
+    def __repr__(self):
+        return '<Kullanici Adi: %s>' % self.Username
 
 @app.route("/")
 def index():
@@ -19,9 +33,11 @@ def main():
     if request.method == 'POST':
         userName = request.form['nm']
         userPass = request.form['pw']
-        if userName in _Dictionary:
-            if userPass == _Dictionary[userName]:
-                return redirect(url_for('redirectUser', name = userName))
+        _User = Users.query.filter_by( Username = userName ).first()
+      ##  if _User.count() == 1:
+        if userPass == _User.Password:
+            return redirect(url_for('redirectUser', name = userName))
+        return "Hatali Kullanici Adi/Sifre"
     else:
         userName = request.args.get('nm')
         return redirect(url_for('redirectUser', name = userName))
@@ -32,6 +48,18 @@ def redirectUser(name):
         return redirect(url_for('adminPage'))
     else:
         return redirect(url_for('userPage', userName = name))
+
+@app.route("/register/")
+def registerUser():
+    return render_template('register.html')
+
+@app.route("/post_user", methods = ['POST'])
+def post_user():
+##    _User = User('Username', 'Password')
+    _User = Users(request.form["Username"], request.form["Password"])
+    db.session.add(_User)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 @app.route("/admin_page/")
 def adminPage():
