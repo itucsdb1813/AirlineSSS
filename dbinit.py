c514af165b34adbd173351ac839dd4c9e44c9e18
Selim Enes Kılıçaslan <33657157+SelimEnes@users.noreply.github.com>
2018-10-21 03:07:41 +0300

    User Operations - General Development
    -Master Page created
    -Login and registration forms are created
    -User and admin menus are created
    -Admins can change user data and post news
    -Related pages and database tables are created
    -dbinit.py file is updated

--- dbinit.py
+++ dbinit.py
@@ -6,8 +6,43 @@ import psycopg2 as dbapi2
 DATABASE_URL = 'postgres://ddzwibxvysqwgx:9e0edae8756536ffdba78314ebde69e2d019e58a2c05dfbad508b5eb657ac9e7@ec2-54-247-101-205.eu-west-1.compute.amazonaws.com:5432/d8o6dthnk5anke'
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+    """
+        CREATE TABLE IF NOT EXISTS person
+            (
+                username character varying(20) NOT NULL,
+                fullname character varying(50) NOT NULL,
+                emailaddress character varying(70) NOT NULL,
+                userrole "char" NOT NULL,
+                balance numeric(7,2) NOT NULL DEFAULT 0,
+                CONSTRAINT person_pkey PRIMARY KEY (username),
+                CONSTRAINT person_fkey FOREIGN KEY (username)
+                    REFERENCES users (username)
+                    ON UPDATE RESTRICT
+                    ON DELETE CASCADE
+            )
+    """,
+    """
+        CREATE TABLE IF NOT EXISTS users
+            (
+                username character varying(20) NOT NULL,
+                password character varying(50) NOT NULL,
+                CONSTRAINT users_pkey PRIMARY KEY (username)
+            )
+    """,
+    """
+        CREATE TABLE IF NOT EXISTS posts
+            (   postid integer NOT NULL DEFAULT nextval('posts_postid_seq'::regclass),
+                poster character varying(20) NOT NULL,
+                content character varying(400) NOT NULL,
+                date date,
+                "time" time without time zone,
+                CONSTRAINT posts_pkey PRIMARY KEY (postid),
+                CONSTRAINT posts_fkey FOREIGN KEY (poster)
+                    REFERENCES users (username)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+            )
+    """,
 ]
 
 
--- server.py
+++ server.py
@@ -1,56 +1,314 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, session, flash
 import psycopg2 as dbapi2
+import datetime
 
 
 app = Flask(__name__)
-##app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ddzwibxvysqwgx:9e0edae8756536ffdba78314ebde69e2d019e58a2c05dfbad508b5eb657ac9e7@ec2-54-247-101-205.eu-west-1.compute.amazonaws.com:5432/d8o6dthnk5anke'
-##app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:itucspw@localhost:65530/itucsdb'
 app.debug = True
-##db = SQLAlchemy(app)
-
+app.secret_key = "secretkey123"
 
 dsn = """user='ddzwibxvysqwgx' password='9e0edae8756536ffdba78314ebde69e2d019e58a2c05dfbad508b5eb657ac9e7'
          host='ec2-54-247-101-205.eu-west-1.compute.amazonaws.com' port=5432 dbname='d8o6dthnk5anke'"""
 
+class User:
+    def __init__(self, uname, fname, email, balance):
+        self.username = uname
+        self.fullname = fname
+        self.email = email
+        self.balance = balance
+
+
 @app.route("/")
 def index():
-    return render_template('main.html')
+    refreshUserData()
+    return render_template('index.html')
 
-@app.route("/flights/<int:code>/")
-def flight(code):
+@app.route("/login", methods = ['POST'])
+def login():
+    _Username = request.form['username']
+    _Password = request.form['password']
     try:
         connection = dbapi2.connect(dsn)
         cursor = connection.cursor()
-        statement = """SELECT f."FlightID", air."AirportName", air."City", x."PlaneModel" From flights as f
-            inner join planes as x on x."PlaneID" = f."PlaneID"
-            inner join airports as air on air."AirportID" = f."DestinationID"
-            WHERE f."FlightID" = %d
-        """ % code
+        statement = """SELECT * FROM users WHERE username = '%s'
+        """ % _Username
         cursor.execute(statement)
-        row = cursor.fetchall()
-        return render_template('flights.html', flights = row)
+        if cursor.rowcount > 0:
+            password = cursor.fetchone()[1]
+            if password == _Password:
+                session['online'] = 1
+                session['Username'] = _Username
+                statement = """SELECT * FROM person WHERE username = '%s'
+                """ % _Username
+                cursor.execute(statement)
+                row = cursor.fetchone()
+                session['Fullname'] = row[1]
+                session['Email'] = row[2]
+                session['Role'] = row[3]
+                session['Balance'] = str(row[4])
+                if ifAdmin():
+                    return redirect(url_for('adminpage'))
+                else:
+                    return redirect(url_for('userpage'))
+        return redirect(url_for('errorpage', message = 'Wrong username or password!'))
     except dbapi2.DatabaseError:
         connection.rollback()
         return "Hata!"
     finally:
         connection.close()
 
-@app.route("/flights/")
-def flights():
+@app.route("/register", methods = ['POST'])
+def register():
+    _Username = request.form['username']
+    _Password = request.form['password']
+    _Fullname = request.form['name']
+    _Email    = request.form['email']
+    connection = dbapi2.connect(dsn)
+    cursor = connection.cursor()
+    statement = """SELECT * FROM users WHERE username = '%s'
+    """ % _Username
+    cursor.execute(statement)
+    if cursor.rowcount > 0:
+        return redirect(url_for('errorpage', message = 'This username already exists!'))
+
+    statement = """INSERT INTO users (username, password) VALUES (%s, %s)
+    """
+    cursor.execute(statement, (_Username, _Password))
+    statement = """INSERT INTO person (username, fullname, emailaddress, userrole) VALUES (%s, %s, %s, %s)
+    """
+    cursor.execute(statement, (_Username, _Fullname, _Email, 'P'))
+    connection.commit()
+    session['online']   = 1
+    session['Username'] = _Username
+    session['Fullname'] = _Fullname
+    session['Email'] = _Email
+    session['Balance'] = 0
+    return redirect(url_for('userpage'))
+
+@app.route("/userpage/")
+def userpage():
+    if 'online' in session:
+        refreshUserData()
+        return render_template('userpage.html')
+    else:
+        return redirect(url_for('errorpage', message = 'You need to log in first!'))
+
+@app.route("/logout")
+def logout():
+    if session['online'] == 1:
+        session.clear()
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('errorpage', message = 'You need to log in first!'))
+
+@app.route("/adminpage")
+def adminpage():
+    if ifAdmin():
+        return render_template('adminpage.html')
+    else:
+        return redirect(url_for('errorpage', message = 'You are not authorized!'))
+
+@app.route("/adm_users")
+def adm_users():
+    if ifAdmin():
+        try:
+            connection = dbapi2.connect(dsn)
+            cursor = connection.cursor()
+            username = session['Username']
+            statement = """SELECT * FROM person WHERE username <> '%s'
+            """ % username
+            cursor.execute(statement)
+            rows = cursor.fetchall()
+            return render_template('adm_users.html', userlist = rows)
+        except dbapi2.DatabaseError as e:
+            connection.rollback()
+            return e
+        finally:
+            connection.close()
+    else:
+        return redirect(url_for('errorpage', message = 'You are not authorized!'))
+
+@app.route("/adm_users/<username>")
+def updateuser(username):
+    if ifAdmin():
+        try:
+            connection = dbapi2.connect(dsn)
+            cursor = connection.cursor()
+            statement = """SELECT * FROM person WHERE username = '%s'
+            """ % username
+            cursor.execute(statement)
+            row = cursor.fetchone()
+            return render_template('adm_updateuser.html', user = row)
+        except dbapi2.DatabaseError as e:
+            connection.rollback()
+            return e
+        finally:
+            connection.close()
+    else:
+        return redirect(url_for('errorpage', message = 'You are not authorized!'))
+
+@app.route('/adm_updateuser/<username>', methods = ['POST'])
+def adm_updateuser(username):
+    if ifAdmin():
+        try:
+            form_dict = request.form
+            connection = dbapi2.connect(dsn)
+            cursor = connection.cursor()
+
+            statement = """UPDATE person SET 
+            """
+            set_text = ""
+            if 'fname_cb' in form_dict and form_dict['fname_cb']:
+                set_text += ("fullname = '%s'" % form_dict['fullname']) + ','
+            if 'mail_cb' in form_dict and form_dict['mail_cb']:
+                set_text += ("emailaddress = '%s'" % form_dict['mail']) + ','
+            if 'role_cb' in form_dict and form_dict['role_cb']:
+                set_text += ("userrole = '%s'" % form_dict['role']) + ','
+            if 'balance_cb' in form_dict and form_dict['balance_cb']:
+                set_text += ("balance = '%s'" % form_dict['balance']) + ','
+            if len(set_text) > 0:
+                set_text = set_text[:-1]
+                statement += set_text
+                statement += " WHERE username = '%s'" % username
+            else:
+                return redirect(url_for('adm_users'))
+            cursor.execute(statement)
+            connection.commit()
+            return redirect(url_for('adm_users'))
+        except dbapi2.DatabaseError as e:
+            connection.rollback()
+            return e
+        finally:
+            connection.close()
+    else:
+        return redirect(url_for('errorpage', message = 'You are not authorized!'))
+
+@app.route("/adm_flights")
+def adm_flights():
+    if ifAdmin():
+        try:
+            connection = dbapi2.connect(dsn)
+            cursor = connection.cursor()
+            statement = """SELECT * FROM users WHERE username <> %s
+            """
+            cursor.execute(statement, str(session['Username']))
+            rows = cursor.fetchall()
+            return render_template('adm_users.html', userlist = rows)
+        except dbapi2.DatabaseError:
+            connection.rollback()
+            return "Hata!"
+        finally:
+            connection.close()
+    else:
+        return redirect(url_for('errorpage', message = 'Not Authorized!'))
+
+@app.route('/errorpage/<message>')
+def errorpage(message):
+    return  render_template('errorpage.html', message = message)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+def ifAdmin():
+    if 'Username' in session:
+        _Refreshed = refreshUserData()
+        if _Refreshed:
+            if session['Role'] == 'A':
+                return True
+        else:
+            return _Refreshed
+    return False
+
+def refreshUserData():
+    if 'Username' in session:
+        try:
+            connection = dbapi2.connect(dsn)
+            cursor = connection.cursor()
+            statement = """SELECT * FROM person WHERE username = '%s'
+            """ % session['Username']
+            cursor.execute(statement)
+            row = cursor.fetchone()
+            session['Fullname'] = row[1]
+            session['Email'] = row[2]
+            session['Role'] = row[3]
+            session['Balance'] = str(row[4])
+            return True
+        except dbapi2.DatabaseError as e:
+            connection.rollback()
+            return e
+        finally:
+            connection.close()
+    else:
+        return False
+
+@app.route('/deleteuser/<username>', methods=['post'])
+def deleteuser(username):
+    if ifAdmin():
+        try:
+            connection = dbapi2.connect(dsn)
+            cursor = connection.cursor()
+            statement = """DELETE FROM users WHERE username = '%s'
+            """ % username
+            cursor.execute(statement)
+            statement = """DELETE FROM person WHERE username = '%s'
+            """ % username
+            cursor.execute(statement)
+            connection.commit()
+            return render_template('adm_users.html')
+        except dbapi2.DatabaseError:
+            connection.rollback()
+            return "Hata!"
+        finally:
+            connection.close()
+    else:
+        return redirect(url_for('errorpage', message = 'Not Authorized!'))
+
+@app.route('/news')
+def news():
     try:
         connection = dbapi2.connect(dsn)
         cursor = connection.cursor()
-        statement = """SELECT f."FlightID", air."AirportName", air."City", x."PlaneModel" From flights as f
-            inner join planes as x on x."PlaneID" = f."PlaneID"
-            inner join airports as air on air."AirportID" = f."DestinationID"
+        statement = """SELECT p.postid, f.fullname, p.poster, p.content, p.date, p.time, f.userrole FROM posts as p
+                    LEFT OUTER JOIN person as f ON f.username = p.poster 
+                    ORDER BY p.postid DESC
         """
         cursor.execute(statement)
-        rows = cursor.fetchall()
-        return render_template('flights.html', flights=rows)
+        posts = cursor.fetchall()
+
+        return render_template('news.html', posts = posts)
+
     except dbapi2.DatabaseError:
         connection.rollback()
         return "Hata!"
     finally:
         connection.close()
+
+@app.route('/sendpost', methods = ['post'])
+def sendpost():
+    refreshUserData()
+    if ifAdmin():
+        _Datetime = datetime.datetime.now()
+        poster = session['Username']
+        content = request.form['content']
+        date = _Datetime.strftime("%d/%m/%Y")
+        time = _Datetime.strftime("%H:%M")
+        connection = dbapi2.connect(dsn)
+        cursor = connection.cursor()
+        statement = """INSERT INTO posts (poster, content, date, time) VALUES (%s, %s, TO_DATE(%s, 'DD/MM/YYYY'), %s)
+        """
+        cursor.execute(statement, (poster, content, date, time))
+        connection.commit()
+    else:
+        return redirect(url_for('errorpage', message = 'Not Authorized!'))
+    return redirect(url_for('news'))
+
+@app.route('/adm_sendpost')
+def adm_sendpost():
+    refreshUserData()
+    if ifAdmin():
+        return render_template('/adm_sendpost.html')
+    else:
+        return redirect(url_for('errorpage', message = 'Not Authorized!'))
 if __name__ == "__main__":
     app.run()
